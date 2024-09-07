893aa13dc4a88762a5f4c2afefda08c0d24a7912
unknown <ngoumkwa.kenne@gmail.com>
2024-09-07 07:23:07 +0100

    Finalisation

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import DarkModeProvider from './context/DarkModeContext.jsx'
 import CountryContextProvider from './context/CountryContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -11,9 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
   <CountryContextProvider>
-  <DarkModeProvider>
     <App />
-  </DarkModeProvider>
   </CountryContextProvider>
   </BrowserRouter>
   </StrictMode>
