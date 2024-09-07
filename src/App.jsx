cd2f5b3e654de0b75e04fa747b10db202fdb071b
unknown <ngoumkwa.kenne@gmail.com>
2024-09-07 08:48:09 +0100

    Finalisation

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,19 @@ import Navbar from "./components/Navbar";
 function App() {
   const {loading}=useContext(CountryContext)
   if (loading) {
-    return (<div>
-            <span className="loading loading-spinner text-primary"></span>
-            <span className="loading loading-spinner text-secondary"></span>
-            <span className="loading loading-spinner text-accent"></span>
-            <span className="loading loading-spinner text-neutral"></span>
-            <span className="loading loading-spinner text-info"></span>
-            <span className="loading loading-spinner text-success"></span>
-            <span className="loading loading-spinner text-warning"></span>
-            <span className="loading loading-spinner text-error"></span>
-    </div>)
+    return (
+      <div className="flex items-center justify-center w-full h-full min-h-screen">
+      <span className="loading loading-spinner text-primary"></span>
+      <span className="loading loading-spinner text-secondary"></span>
+      <span className="loading loading-spinner text-accent"></span>
+      <span className="loading loading-spinner text-neutral"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-error"></span>
+    </div>
+    
+    )
   }
   return (
     <>
